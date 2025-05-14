fd20e576e4befdce7eb85f0d3752fba34cad210a
wake0p <wake0p@wake0pdeMacBook-Air.local>
2025-05-14 16:17:05 +0800

    init project

---
+++ src/test/java/com/baidu/netdisk/NetdiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baidu.netdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetdiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
