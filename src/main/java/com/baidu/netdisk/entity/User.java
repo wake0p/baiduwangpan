246f4600400744c545add76f013fd749d91320a4
xiayutian121 <726417697@qq.com>
2025-06-16 15:44:01 +0800

    modify user

--- src/main/java/com/baidu/netdisk/entity/User.java
+++ src/main/java/com/baidu/netdisk/entity/User.java
@@ -1,10 +1,16 @@
 package com.baidu.netdisk.entity;
 
 import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
-
 @Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
@@ -16,4 +22,16 @@ public class User {
     private Integer status;     // 状态：0-禁用，1-正常
     private LocalDateTime createTime;
     private LocalDateTime updateTime;
+    private String employeeId;  // 工号
+    private String department;  // 部门
+    private String position;    // 职位
+    private String companyEmail; // 公司邮箱
+    private Integer isAdmin = 0;    //管理员：1-是，0-不是，默认0
+    private String nickname;       //昵称
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
