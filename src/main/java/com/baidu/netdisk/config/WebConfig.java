815c5a1e05ac4eb9eb167d1df891bbf2e6c4c28f
Invictu4S <15224971+invictu4s@user.noreply.gitee.com>
2025-07-01 14:17:47 +0800

    fix bug

---
+++ src/main/java/com/baidu/netdisk/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.baidu.netdisk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 或指定 http://localhost:5173
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
